837e84a123a00bb511fc808511ef6351624f8ae9
pravinshinde <pravin86.shinde@gmail.com>
2015-05-21 15:43:11 +0530

    Send Email without Intent in Android.
    This is a Library project. With the help of this, user can able to send
    emails to single or multiple recipients with attachment.

---
+++ src/com/infostretch/sendmail/MainActivity.java
@@ -0,0 +1,18 @@
+package com.infostretch.sendmail;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	private Mail m;
+	/** Called when the activity is first created. */
+	@Override 
+	public void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState); 
+		setContentView(R.layout.activity_main); 
+		
+	}
+ 
+	
+}
